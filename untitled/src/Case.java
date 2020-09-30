b8a8e15415df4210cac8fa6ccc6c16290d19a423
Ziinou19 <64192474+Ziinou19@users.noreply.github.com>
2020-09-30 10:39:30 +0200

    Update Case.java

--- untitled/src/Case.java
+++ untitled/src/Case.java
@@ -9,5 +9,5 @@ public class Case {
     Tant que i est inferieur a la taille du tableau
     increment le i
         tableau a 2 dimension 0 c la ligne 0 j
-        IL RESTERA TOUJOUR A LA PLACE 0MAIS J SE DEPLACE ET LES 100 REMPLACE
-        0
+       
+       
